67e340ba7a60803b31246baf14a0ae0480aed279
HirokiOmote <bubo@bubo.jp>
2017-11-30 18:16:16 +0900

    Update animation script

--- src/js/modules/aos.js
+++ src/js/modules/aos.js
@@ -1,10 +1,10 @@
 import AOS from 'aos';
-// import slick from './slick';
 
 const aosInit = new AOS.init({
   once: true,
-  duration: 1000,
+  duration: 800,
   offset: 300,
+  delay: 150,
   startEvent: 'load'
 });
 
