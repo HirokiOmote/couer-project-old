101a74bafd7c124afd319948f54e4d00ff03a1a2
HirokiOmote <bubo@bubo.jp>
2018-01-31 19:10:29 +0900

    Change bs-config.js

--- bs-config.js
+++ bs-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  files: "./wordpress/**/*.css, ./wordpress/**/*.js, ./wordpress/**/*.php",
+  files: "./wordpress/wp-content/themes/**/*.css, ./wordpress/wp-content/themes/**/*.js, ./wordpress/wp-content/themes/**/*.php",
   injectChanges: true,
-  proxy: "http://www.coeur-project.dev"
+  proxy: "http://www.coeur-project.test"
 }
