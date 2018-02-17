f1d930bb0bebf10156bd6cbc7c454e3f68023734
HirokiOmote <bubo@bubo.jp>
2018-02-17 16:56:27 +0900

    Add profile & contact pages

---
+++ src/profile.php
@@ -0,0 +1,434 @@
+<?php
+  get_header();
+  // Template Name: 会社情報
+?>
+
+<main class="profile">
+  <header class="header">
+    <h1>
+      <picture>
+        <source media="min-width: 768px;" srcset="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/guide/ttl_header@pc.svg">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/guide/ttl_header@sp.svg" alt="会社情報">
+      </picture>
+    </h1>
+  </header>
+
+  <div class="wrapper">
+    <section class="section-profile">
+      <header>
+        <p>COMPANY PROFILE</p>
+        <h2>会社情報</h2>
+      </header>
+
+      <div class="flex-column">
+        <div class="half-column">
+          <section class="project">
+            <h3>株式会社クールプロジェクト</h3>
+
+            <div class="table">
+              <table>
+                <tbody>
+                  <tr>
+                    <th>商　号</th>
+                    <td>株式会社クールプロジェクト</td>
+                  </tr>
+                  <tr>
+                    <th>代表取締役</th>
+                    <td>茂藤 雅彦</td>
+                  </tr>
+                  <tr>
+                    <th>本社所在地</th>
+                    <td>〒921-8802 石川県野々市市押野1-380 クールセントラルビル</td>
+                  </tr>
+                  <tr>
+                    <th>電話/FAX</th>
+                    <td>Tel.076-246-5111 Fax.076-246-5113</td>
+                  </tr>
+                  <tr>
+                    <th>設　立</th>
+                    <td>平成14年9月</td>
+                  </tr>
+                  <tr>
+                    <th>資本金</th>
+                    <td>2,000万円</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <p><b>［金沢本社 / 管理部］ </b><br>
+            〒921-8802 石川県野々市市押野1-380<br>
+            クールセントラルビル<br>
+            Tel.076-246-5111 Fax.076-246-5113</p>
+            <p><b>［東京支社］</b><br>
+            〒104-0061 東京都中央区銀座5-9-12<br>
+            ダイヤモンドビル4Ｆ<br>
+            Free.0120-010-510  Tel.03-3572-4300<br>
+            Fax.03-3572-4301</p>
+            <p><b>［大阪支社］</b><br>
+            〒 541-0054 大阪府大阪市中央区南本町4丁目<br>
+            3-16 ブランズタワー御堂筋本町404<br>
+            Tel&Fax.06-7171-0044</p>
+          </section>
+
+          <section class="esthetique">
+            <h3>エステティック事業部</h3>
+
+            <div class="table">
+              <table>
+                <tbody>
+                  <tr>
+                    <th>商　号</th>
+                    <td>株式会社クール富山</td>
+                  </tr>
+                  <tr>
+                    <th>本社所在地</th>
+                    <td>〒939-8211 富山県富山市二口町1-11-6 K2ビル</td>
+                  </tr>
+                  <tr>
+                    <th>電話/FAX</th>
+                    <td>Tel.076-491-8830 Fax.076-491-8840</td>
+                  </tr>
+                  <tr>
+                    <th>設　立</th>
+                    <td>平成21年9月</td>
+                  </tr>
+                  <tr>
+                    <th>店舗</th>
+                    <td>クールエステティック富山・二口店</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <div class="table">
+              <table>
+                <tbody>
+                  <tr>
+                    <th>商　号</th>
+                    <td>株式会社クール福井</td>
+                  </tr>
+                  <tr>
+                    <th>本社所在地</th>
+                    <td>〒918-8237 福井県福井市和田東1-2104</td>
+                  </tr>
+                  <tr>
+                    <th>電話/FAX</th>
+                    <td>Tel.0776-30-2772 Fax.0776-30-2773</td>
+                  </tr>
+                  <tr>
+                    <th>設　立</th>
+                    <td>平成21年9月</td>
+                  </tr>
+                  <tr>
+                    <th>店舗</th>
+                    <td>クールエステティック福井店<br>
+                    クールエステティック鯖江店<br>
+                    クールフェイシャルボーテ福井店</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <div class="table">
+              <table>
+                <tbody>
+                  <tr>
+                    <th>商　号</th>
+                    <td>株式会社クール金沢</td>
+                  </tr>
+                  <tr>
+                    <th>本社所在地</th>
+                    <td>〒933-0014 富山県高岡市野村1536-17 アーバンビル2F</td>
+                  </tr>
+                  <tr>
+                    <th>電話/FAX</th>
+                    <td>Tel.0766-28-8623 Fax.0766-28-8624</td>
+                  </tr>
+                  <tr>
+                    <th>設　立</th>
+                    <td>平成25年9月</td>
+                  </tr>
+                  <tr>
+                    <th>店舗</th>
+                    <td>クールエステティック高岡店</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <div class="table">
+              <table>
+                <tbody>
+                  <tr>
+                    <th>商　号</th>
+                    <td>株式会社クール高岡</td>
+                  </tr>
+                  <tr>
+                    <th>本社所在地</th>
+                    <td>〒933-0014 富山県高岡市野村1536-17 アーバンビル2F</td>
+                  </tr>
+                  <tr>
+                    <th>電話/FAX</th>
+                    <td>Tel.0766-28-8623 Fax.0766-28-8624</td>
+                  </tr>
+                  <tr>
+                    <th>設　立</th>
+                    <td>平成25年9月</td>
+                  </tr>
+                  <tr>
+                    <th>店舗</th>
+                    <td>クールエステティック高岡店</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </section>
+
+          <section class="fc">
+            <h3>FC(フランチャイズ)事業部</h3>
+            <p><b>［金沢本社］</b><br>
+            〒921-8802 石川県野々市市押野1-380 クールセントラルビル<br>
+            Tel.076-246-2060 Fax.076-246-2061</p>
+          </section>
+        </div>
+
+        <div class="half-column">
+          <section class="consulting">
+            <h3>コンサルティング事業部</h3>
+            <p><b>［金沢本社］ </b>
+            〒921-8802 石川県野々市市押野1-380 クールセントラルビル<br>
+            Tel.076-246-2060 Fax.076-246-2061</p>
+          </section>
+
+          <section class="products">
+            <h3>ビューティプロダクツ事業部</h3>
+
+            <section>
+              <h4>●PB(プライベートブランド)グループ</h4>
+              <p><b>［金沢本社 / JBマシナリー］ </b><br>
+              〒921-8802 石川県野々市市押野1-380 クールセントラルビル<br>
+              Free.0120-00-7980 Tel.076-248-2700 Fax.076-248-2730</p>
+              <p><b>［東京支社］</b><br>
+              〒104-0061 東京都中央区銀座5-9-12 ダイヤモンドビル4Ｆ<br>
+              Free.0120-010-510  Tel.03-3572-4300 Fax.03-3572-4301</p>
+              <p><b>［大阪支社］</b><br>
+              〒 541-0054 大阪府大阪市中央区南本町4丁目3-16 ブランズタワー御堂筋本町404<br>
+              Tel&Fax.06-7171-0044</p>
+            </section>
+
+            <section>
+              <h4>●OEMグループ</h4>
+              <p><b>［金沢本社］</b><br>〒921-8802 石川県野々市市押野1-380 クールセントラルビル<br>
+               Tel.076-246-2060 Fax.076-246-2061</p>
+            </section>
+
+            <section>
+              <h4>●ディーラーグループ</h4>
+              <p><b>［金沢本社］</b><br>〒921-8802 石川県野々市市押野1-380 クールセントラルビル<br>
+               Tel.076-246-2060 Fax.076-246-2061</p>
+            </section>
+          </section>
+
+          <section class="ec">
+            <h3>eコマース事業部</h3>
+            <p><b>［金沢本社］</b><br>〒921-8802 石川県野々市市押野1-380 クールセントラルビル<br>
+             Tel.076-246-2060 Fax.076-246-2061</p>
+          </section>
+
+          <section class="bio">
+            <h3>バイオヘルス事業部</h3>
+            <p><b>［金沢本社］</b><br>〒921-8802 石川県野々市市押野1-380 クールセントラルビル<br>
+             Tel.076-246-2060 Fax.076-246-2061</p>
+          </section>
+
+          <section class="overseas">
+            <h3>海外事業部</h3>
+            <p><b>［東京支社］</b><br>
+            〒104-0061 東京都中央区銀座5-9-12 ダイヤモンドビル4Ｆ<br>
+            Free.0120-010-510  Tel.03-3572-4300 Fax.03-3572-4301</p>
+            <p><b>［クールエステティック富山研修センター］</b><br>
+            〒939-8015 富山県富山市中川原382-5</p>
+          </section>
+        </div>
+      </div>
+
+      <div class="flex-column">
+        <figure>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/profile/img_profile001.png" alt="クールグループの全国展開">
+        </figure>
+
+        <figure>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/profile/img_profile002.png" alt="クールグループ年間売上額推移">
+        </figure>
+      </div>
+    </section>
+
+    <section class="history">
+      <header>
+        <p>COMPANY HISOTRY</p>
+        <h2>グループ沿革</h2>
+      </header>
+
+      <table>
+        <tbody>
+          <tr>
+            <th>2002年09月</th>
+            <td>株式会社クールプロジェクト設立（石川県金沢市）</td>
+          </tr>
+          <tr>
+            <th>2002年10月</th>
+            <td>クールエステティック旧金沢店（金沢駅西口前） OPEN</td>
+          </tr>
+          <tr>
+            <th>2003年09月</th>
+            <td>クールエステティック 旧富山店（富山駅北口前） OPEN</td>
+          </tr>
+          <tr>
+            <th>2005年07月</th>
+            <td>クールエステティック高岡店  OPEN（富山県内2号店）</td>
+          </tr>
+          <tr>
+            <th>2006年04月</th>
+            <td>クールエステティック 福井店  OPEN</td>
+          </tr>
+          <tr>
+            <th>2006年08月</th>
+            <td>クールエステティック富山店 3倍増床リニューアルOPEN</td>
+          </tr>
+          <tr>
+            <th>2007年04月</th>
+            <td>クールエステティック富山・山室店 OPEN（富山県内2号店）</td>
+          </tr>
+          <tr>
+            <th>2008年01月</th>
+            <td>クールエステティック新金沢店 兼 本社事務所  OPEN</td>
+          </tr>
+          <tr>
+            <th>2009年01月</th>
+            <td>クールエステティック 高岡店 1.5倍増床リニューアルOPEN</td>
+          </tr>
+          <tr>
+            <th>2009年06月</th>
+            <td>クールエステティック富山・山室店　増床リニューアルOPEN</td>
+          </tr>
+          <tr>
+            <th>2010年05月</th>
+            <td>クールエステティック富山・駅前店→富山・市民プラザ店  移転OPEN</td>
+          </tr>
+          <tr>
+            <th>2010年08月</th>
+            <td>クールエステティック 鯖江店OPEN（福井県内2号店）</td>
+          </tr>
+          <tr>
+            <th>2010年12月</th>
+            <td>ジェイ・ビー・マシナリー設立（美容機器部門）</td>
+          </tr>
+          <tr>
+            <th>2011年12月</th>
+            <td>ジェイ・ビー・マシナリー　東京支社OPEN</td>
+          </tr>
+          <tr>
+            <th>2012年03月</th>
+            <td>クールエステティック金沢店　リニューアルOPEN</td>
+          </tr>
+          <tr>
+            <th>2012年08月</th>
+            <td>クールエステティック 富山・山室店　外観・外溝リニューアルOPEN</td>
+          </tr>
+          <tr>
+            <th>2012年09月</th>
+            <td>クールエステティック富山・市民プラザ店 2倍増床リニューアルOPEN  ANNEX誕生</td>
+          </tr>
+          <tr>
+            <th>2013年05月</th>
+            <td>ジェイ・ビー・マシナリー東京支社 移転OPEN</td>
+          </tr>
+          <tr>
+            <th>2013年11月</th>
+            <td>クールエステティック　マレーシア（クアラルンプール）店OPEN</td>
+          </tr>
+          <tr>
+            <th>2014年04月</th>
+            <td>クールエステティック 富山・二口店 OPEN（富山・市民プラザ店移転）</td>
+          </tr>
+          <tr>
+            <th>2014年11月</th>
+            <td>クールエステティックがセレクトしたビューティ通販サイト「ココセレ」OPEN</td>
+          </tr>
+          <tr>
+            <th>2015年01月</th>
+            <td>クールプロジェクト本社移転</td>
+          </tr>
+          <tr>
+            <th>2015年02月</th>
+            <td>痩身専門サロンクールグランボディ金沢店OPEN</td>
+          </tr>
+          <tr>
+            <th>2015年06月</th>
+            <td>フェイシャル専門サロンクールフェイシャルボーテ金沢店OPEN</td>
+          </tr>
+          <tr>
+            <th>2015年12月</th>
+            <td>まつげエスクテ専門店クールアイラッシュ金沢店OPEN</td>
+          </tr>
+          <tr>
+            <th>2016年08月</th>
+            <td>クールエステティック・ビューティショップ（名古屋・丸栄）OPEN</td>
+          </tr>
+          <tr>
+            <th>2016年09月</th>
+            <td>クールフェイシャルボーテ福井店OPEN</td>
+          </tr>
+          <tr>
+            <th>2017年04月</th>
+            <td>クールプロジェクト東京支社　銀座に移転OPEN</td>
+          </tr>
+          <tr>
+            <th>2017年12月</th>
+            <td>クールプロジェクト大阪支社　京都から大阪に移転OPEN</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+
+    <section class="mark">
+      <header>
+        <p>REGISTERED TRADEMARK</p>
+        <h2>登録商標</h2>
+      </header>
+
+      <p>株式会社クールプロジェクトが権利を有する<br>登録商標です。</p>
+
+      <ul>
+        <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_coeur_project.svg" alt=""></a></li>
+        <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_coeur_grandbody.svg" alt=""></a></li>
+        <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_coeur_grandbody02.svg" alt=""></a></li>
+        <li><a href=""><img srcset="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_jbm.png, <?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_jbm@2x.png 2x" src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_jbm.png" alt=""></a></li>
+        <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_dr_caviet.svg" alt=""></a></li>
+        <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_the_zeur.svg" alt=""></a></li>
+        <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_beaute_de_coeur.svg" alt=""></a></li>
+        <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_pico_connna.svg" alt=""></a></li>
+        <li><a href=""><img srcset="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_kokosere.png, <?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_kokosere@2x.png 2x" src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_kokosere.png" alt=""></a></li>
+        <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_air_resh.svg" alt=""></a></li>
+        <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_dhs.svg" alt=""></a></li>
+        <li><a href=""><img srcset="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_nihon.png, <?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_nihon@2x.png 2x" src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_nihon.png" alt=""></a></li>
+        <li><a href=""><img srcset="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_nihon002.png, <?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_nihon002@2x.png 2x" src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_nihon002.png" alt=""></a></li>
+        <li><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/common/icon_coeur_project.svg" alt=""></a></li>
+      </ul>
+    </section>
+
+    <div class="footer-slide">
+      <ul>
+        <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/profile/img_slide001.jpg" alt=""></li>
+        <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/profile/img_slide002.jpg" alt=""></li>
+        <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/profile/img_slide003.jpg" alt=""></li>
+        <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/profile/img_slide004.jpg" alt=""></li>
+        <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/profile/img_slide005.jpg" alt=""></li>
+        <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/profile/img_slide006.jpg" alt=""></li>
+      </ul>
+    </div>
+  </div>
+</main>
+
+<?php get_footer(); ?>
