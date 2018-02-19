9e60879a5fe9c06e4472de43cf8667de1a93af85
HirokiOmote <bubo@bubo.jp>
2018-02-19 18:12:15 +0900

    Fix title in media page

--- src/media.php
+++ src/media.php
@@ -7,8 +7,8 @@ get_header();
   <header class="header">
     <h1>
       <picture>
-        <source media="min-width: 768px;" srcset="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/recruit/ttl_header@pc.svg">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/recruit/ttl_header@sp.svg" alt="採用情報">
+        <source media="min-width: 768px;" srcset="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/media/ttl_header@pc.svg">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pages/media/ttl_header@sp.svg" alt="メディア">
       </picture>
     </h1>
   </header>
